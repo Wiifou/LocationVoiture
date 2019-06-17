dcaaa17762e7ec49afe72db61cddb8dcc2357b33
JejeB <34520004+JejeB@users.noreply.github.com>
2019-06-17 09:07:48 +0200

    Add files via upload

---
+++ vehicules.h
@@ -0,0 +1,14 @@
+#ifndef VEHICULES_H
+#define VEHICULES_H
+
+
+typedef struct vehicules{
+	char immat[10];
+	char marque[15];
+	char modele[15];
+	short int millesime;
+	int kilometrage;
+	char categorie;
+}vehicules;
+
+#endif
