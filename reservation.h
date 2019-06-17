a669f7933151db98a3ba31b860f54eb1efb58686
JejeB <34520004+JejeB@users.noreply.github.com>
2019-06-17 09:08:06 +0200

    Add files via upload

---
+++ reservation.h
@@ -0,0 +1,17 @@
+#ifndef RESERVATION_H
+#define RESERVATION_H
+
+#include"vehicules.h"
+#include"client.h"
+
+typedef struct reservations{
+	int num_reserv;
+	int date_debut[3];
+	int date_fin[3];
+	long int num_permis;
+	char immat[10];
+	vehicules *veve;
+	clients *client;
+}reservations;
+
+#endif
